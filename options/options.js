7d1c68345a139f2371d4e1aadb4039fa8b011293
Crowdbotics <team@crowdbotics.com>
2024-01-03 16:20:10 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-river-45259",
+  url: "https://black-river-45259.botics.co",
+  api: "https://black-river-45259.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
